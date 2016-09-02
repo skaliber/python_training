b339fa8c7635561004780a8eb9544129c1568d0c
Toader Mihali <Toader.Mihali@wintech.local>
2016-09-02 15:52:48 +0100

    with exceptions handleler

--- operators/calculate_factorial.py
+++ operators/calculate_factorial.py
@@ -2,5 +2,10 @@ from math import factorial as fact
 
 
 def calc_factorial(number):
-    results = fact(number)
-    return results
+    if number < 0:
+        print("Sorry I can't do it for negative numbers")
+    elif number == 0:
+        print("The factorial of 0 is 1")
+    elif number > 1:
+        print(fact(number))
+
--- test_factorial.py
+++ test_factorial.py
@@ -2,9 +2,5 @@ from operators import calculate_factorial as fact
 
 
 number = int(input("Enter a number: "))
-if number < 0:
-    print("Sorry I can't do it for negative numbers")
-elif number == 0:
-    print("The factorial of 0 is 1")
-elif number > 1:
-    print(fact.calc_factorial(number))
+
+fact.calc_factorial(number)
