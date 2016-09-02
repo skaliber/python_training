3163a34b51cae64d6f40881b812341c1725724b3
Toader Mihali <Toader.Mihali@wintech.local>
2016-09-02 15:33:50 +0100

    assign value in a string

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from operators import methods as meth
 
 def main():
     meth.go()
-
+    print("This only executes when %s is executed rather than imported" % __file__)
 
 if __name__ == "__main__": main()
+
+
